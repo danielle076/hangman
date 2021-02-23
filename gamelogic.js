578bed49827219c81129c84c6f1cac56666dbab2
danielle076 <d.vandenakker@novi-education.nl>
2021-02-23 17:24:12 +0100

    added functie displaywordsofar

--- gamelogic.js
+++ gamelogic.js
@@ -42,9 +42,44 @@ function displayWordSoFar(word, guesses) {
 //       .join(""); // "j a _ a _ _ _ _ _ _"
 // }
 
+// FUNCTIE ISGAMEWON
+
+// Kunnen we 1 letter vinden die je niet hebt geraden? return false.
+// Kunnen we niet 1 letter vinden die je niet hebt geraden return true.
+
+// Stappenplan
+// • Loggen: wat is "word", wat is "guesses"
+// • Word -> word.split(""): ["j", "a", "v", "a", "s", "c", "r", "i", "p", "t"]
+// • Loopen over de array met de letters van het woord
+// • Voor elke letter in het woord
+// • Zit deze letter in guesses -> guesses.includes(letter)
+// • if in de loop
+// • Is guesses true -> we gaan door
+// • Is guesses false -> we stoppen: return false
+// • Zijn we aan het einde van de loop? Aan het einde van de functie return true
+
 function isGameWon(word, guesses) {
-  // WRITE ME
+  const lettersInWordArray = word.split("");
+
+  for (let index = 0; index < lettersInWordArray.length; index++) {
+    const letterInWord = lettersInWordArray[index];
+    const letterGuessed = guesses.includes(letterInWord);
+    if (!letterGuessed) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
+// Korte versie
+// function isGameWon(word, guesses) {
+//   const letterNotGuessed = word
+//       .split("") // // ["j", "a", "v", "a", "s", "c", "r", "i", "p", "t"]
+//       .find((letter) => !guesses.includes(letter));
+//
+//   return letterNotGuessed === undefined; // if there is no letter not guessed, we won
+// }
 
 function isGameLost(word, guesses) {
   // WRITE ME
